e41b0db229b1b1afb15cf8c4eb81cfd42c2caaac
HElnur <huseynove174@gmail.com>
2025-01-01 16:54:58 +0400

    Add get query to type api

---
+++ TrackMyFinance.Application/CQRS/v1/Type/Queries/Get/GetTypeQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackMyFinance.Application.Core;
+using TrackMyFinance.Application.Interfaces;
+using TrackMyFinance.Models.v1.Type.Get;
+
+namespace TrackMyFinance.Application.CQRS.v1.Type.Queries.Get
+{
+    public class GetTypeQueryHandler : IRequestHandler<GetTypeQuery, ApiResult<List<GetTypeResponse>>>
+    {
+        private readonly ITypeService _service;
+
+        public GetTypeQueryHandler(ITypeService service)
+            => _service = service;
+
+        public async Task<ApiResult<List<GetTypeResponse>>> Handle(GetTypeQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _service.Get(request.Request);
+
+            return result;
+        }
+    }
+}
