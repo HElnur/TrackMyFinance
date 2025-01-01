d7ad449674d16a1ddf74133294acc7f2069d43ee
HElnur <huseynove174@gmail.com>
2025-01-01 16:33:51 +0400

    Create CQRS for type api | Add create command

---
+++ TrackMyFinance.Application/CQRS/v1/Type/Commands/Create/CreateTypeCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackMyFinance.Application.Core;
+using TrackMyFinance.Models.v1.Type.Create;
+
+namespace TrackMyFinance.Application.CQRS.v1.Type.Commands.Create
+{
+    public class CreateTypeCommand : IRequest<ApiResult<CreateTypeResponse>>
+    {
+        public CreateTypeRequest Request { get; set; }
+
+        public CreateTypeCommand(CreateTypeRequest request)
+            => Request = request;
+    }
+}
---
+++ TrackMyFinance.Application/Interfaces/ITypeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackMyFinance.Application.Core;
+using TrackMyFinance.Models.v1.Type.Create;
+using TrackMyFinance.Models.v1.Type.Get;
+using TrackMyFinance.Models.v1.Type.Update;
+
+namespace TrackMyFinance.Application.Interfaces
+{
+    public interface ITypeService
+    {
+        Task<ApiResult<CreateTypeResponse>> Create(CreateTypeRequest request);  
+        Task<ApiResult<UpdateTypeResponse>> Update(UpdateTypeRequest request);
+        Task<ApiResult<List<GetTypeResponse>>> Get(GetTypeRequest request);
+        Task<ApiResult<Guid>> Delete(Guid id);
+    }
+}
