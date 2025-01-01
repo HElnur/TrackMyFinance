8e53249e2811e888f37d905247f63e98f5e90350
HElnur <huseynove174@gmail.com>
2025-01-01 16:26:55 +0400

    Add type models

---
+++ TrackMyFinance.Models/v1/Type/Get/GetTypeResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackMyFinance.Models.v1.Type.Get
+{
+    public class GetTypeResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
