9322a9ae105037aaa0868c9a183d1cd9496d9afa
HElnur <huseynove174@gmail.com>
2025-01-02 18:38:16 +0400

    Add type service

---
+++ TrackMyFinance.Infrastructure/Services/TypeService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackMyFinance.Application.Core;
+using TrackMyFinance.Application.Interfaces;
+using TrackMyFinance.Models.v1.Type.Create;
+using TrackMyFinance.Models.v1.Type.Get;
+using TrackMyFinance.Models.v1.Type.Update;
+
+namespace TrackMyFinance.Infrastructure.Services
+{
+    public class TypeService : ITypeService
+    {
+        public Task<ApiResult<CreateTypeResponse>> Create(CreateTypeRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResult<Guid>> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResult<List<GetTypeResponse>>> Get(GetTypeRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApiResult<UpdateTypeResponse>> Update(UpdateTypeRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
