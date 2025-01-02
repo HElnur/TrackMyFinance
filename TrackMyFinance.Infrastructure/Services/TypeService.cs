a0f7194ec16ac5f53a2c1eea2a5523f5cbcd6fcc
HElnur <huseynove174@gmail.com>
2025-01-02 18:46:35 +0400

    Add create method to type service

--- TrackMyFinance.Infrastructure/Services/TypeService.cs
+++ TrackMyFinance.Infrastructure/Services/TypeService.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackMyFinance.Application.Core;
 using TrackMyFinance.Application.Interfaces;
+using TrackMyFinance.Domain.Entities;
+using TrackMyFinance.Domain.Enums;
+using TrackMyFinance.Infrastructure.Data;
 using TrackMyFinance.Models.v1.Type.Create;
 using TrackMyFinance.Models.v1.Type.Get;
 using TrackMyFinance.Models.v1.Type.Update;
@@ -13,9 +17,35 @@ namespace TrackMyFinance.Infrastructure.Services
 {
     public class TypeService : ITypeService
     {
-        public Task<ApiResult<CreateTypeResponse>> Create(CreateTypeRequest request)
+        private readonly ApplicationDbContext _context;
+        public TypeService(ApplicationDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+
+        public async Task<ApiResult<CreateTypeResponse>> Create(CreateTypeRequest request)
+        {
+
+            var existType = await _context.Types.AnyAsync(x => x.Name == request.Name);
+
+            if (existType)
+                return ApiResult<CreateTypeResponse>.Error(ErrorCodes.DATA_IS_ALREADY_EXIST);
+
+            var type = new Domain.Entities.Type
+            {
+                Name = request.Name,                
+            };
+
+            await _context.Types.AddAsync(type);
+            await _context.SaveChangesAsync();
+
+            var response = new CreateTypeResponse
+            {
+                Id = type.Id,
+                Name = type.Name,
+            };
+
+            return ApiResult<CreateTypeResponse>.OK(response);
         }
 
         public Task<ApiResult<Guid>> Delete(Guid id)
